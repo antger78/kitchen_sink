e852fc129c08f7f11aebc024322fd4303986f901
Mark <markloercher@gmail.com>
2022-03-10 17:24:08 -0500

    mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,21 +1,52 @@
-const { Recipe, User } = require("../models");
+const { Recipe, User, Ingredient } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
-	Query: {
-		recipes: async () => {
-			return Recipe.find().sort({ createdAt: -1 });
-		},
+  Query: {
+    recipes: async () => {
+      return Recipe.find().sort({ createdAt: -1 });
+    },
 
-		users: async () => {
-			return User.find();
-		},
-		recipe: async (parent,{_id}) => {
-			return Recipe.findOne({_id});
-		},
+    users: async () => {
+      return User.find();
+    },
+    recipe: async (parent, { _id }) => {
+      return Recipe.findOne({ _id });
+    },
+  },
 
-	},
-
-	// Mutation: {},
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect Credientials");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addRecipe: async (parent, args, context) => {
+      if (context.user) {
+		const ingredients = await Ingredient.create()
+        const recipe = await Recipe.create({
+          author: context.user._id,
+          title: args.title,
+          prepInstructions: args.prepInstructions,
+          prepTime: args.prepTime,
+          difficulty: args.difficulty,
+		  ingredients: ingredients
+        });
+      }
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,45 +1,61 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-	scalar Date
+  scalar Date
 
-	type User {
-		_id: ID
-		username: String
-		password: String
-		email: String
-		recipes: [Recipe]
-	}
+  type User {
+    _id: ID
+    username: String
+    password: String
+    email: String
+    recipes: [Recipe]
+  }
 
-	type Recipe {
-		_id: ID
-		title: String
-		author: String
-		createdAt: Date
-		ingredients: [Ingredient]
-		prepInstructions: String
-		prepTime: Int
-		difficulty: String
-	}
-	type Ingredient {
-		ingredient: String
-		quantity: String
-	}
+  type Recipe {
+    _id: ID
+    title: String
+    author: String
+    createdAt: Date
+    ingredients: [Ingredient]
+    prepInstructions: String
+    prepTime: Int
+    difficulty: String
+    userLikes: [User]
+  }
+  type Ingredient {
+    ingredient: String
+    quantity: String
+  }
 
-	type Query {
-		users: [User]
-		recipes: [Recipe]
-		recipe(recipeId: ID!): Recipe
-	}
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-	# type Mutation {
-	# 	# addUser(username: String!, email: String!, password: String!): User
-	# 	# addRecipe(
-	# 	# 	title: String!
-	# 	# 	author: String!
-	# 	# 	ingredients: [Ingredient]
-	# 	# ): Recipe
-	# }
+  type Query {
+    users: [User]
+    recipes: [Recipe]
+    recipe(_id: ID!): Recipe
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    logIn(email: String!, password: String!): Auth
+    addRecipe(
+      title: String!
+      author: String!
+      ingredients: [Ingredient]
+    ): Recipe
+    deleteRecipe(_id: ID!): Recipe
+    updateRecipe(
+      title: String
+      ingredients: [Ingredient]
+      prepInstructions: String
+      prepTime: Int
+      difficulty: String
+    ): Recipe
+    likeRecipe(_id: ID!): Recipe
+  }
 `;
 
 module.exports = typeDefs;
