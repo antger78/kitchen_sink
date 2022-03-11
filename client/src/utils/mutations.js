124a2f875bcf0945f0fc161d27d83ccceaf8853f
jillsuarez <jillsuarez@live.com>
2022-03-11 12:19:03 -0500

    login and signup functional

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,19 +1,55 @@
 import { gql } from '@apollo/client'
 
 export const MUTATION_ADDUSER = gql`
-
+mutation ($username: String!, $email:String!, $password:String!){
+  addUser(username:$username, email: $email, password: $password) {
+    token
+    user {
+      username
+      email
+    }
+  }
+}
 `;
 export const MUTATION_LOGIN = gql`
-
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token 
+    user{
+      _id
+      username
+      email
+    }
+  }
+}
 `;
-export const MUTATION_ADDRECIPE = gql`
+// export const MUTATION_ADDRECIPE = gql`
+// mutation ($title:String!,
+// 	$prepInstructions:String,
+// 	$prepTime:Int,
+//   $cookTime:Int,
+// 	$difficulty:String,
+// 	$ingredients:[String]) {
+//     addRecipe(title:$title,
+//     	prepInstructions: $prepInstructions,
+//     	prepTime:$prepTime,
+//       cookTime:$cookTime,
+//     	difficulty:$difficulty,
+//       ingredients:$ingredients
+//     ) {
+//       title
+//       prepInstructions
+//       prepTime
+//       difficulty
+//       ingredients 
+//     }
+//   }
+// `;
+// export const MUTATION_DELETERECIPE = gql`
 
-`;
-export const MUTATION_DELETERECIPE = gql`
+// `;
+// export const MUTATION_UPDATERECIPE = gql`
 
-`;
-export const MUTATION_UPDATERECIPE = gql`
+// `;export const MUTATION_LIKERECIPE = gql`
 
-`;export const MUTATION_LIKERECIPE = gql`
-
-`;
+// `;
