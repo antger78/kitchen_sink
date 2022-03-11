b9e255f7ab3711b3cd08221eeb509a6f64a17fff
Anthony Geritano <anthony.geritano@aya.yale.edu>
2022-03-11 17:29:40 -0500

    small additions to search

--- client/src/components/SearchRecipes.js
+++ client/src/components/SearchRecipes.js
@@ -1,3 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import { useMutation } from "@apollo/client";
+import {QUERY_RECIPES} from '../utils/queries';
+
+
