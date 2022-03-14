63bab54d9a7f4ec22882005f88ba8f3c72c475ea
Mark <markloercher@gmail.com>
2022-03-13 20:11:46 -0400

    mapping recipe array to cards

--- client/src/components/RecipeList/index.js
+++ client/src/components/RecipeList/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, ListGroup, Row } from "react-bootstrap";
 import { useQuery } from "@apollo/client";
 import { QUERY_RECIPES } from "../../utils/queries";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const RecipeList = (props) => {
   // get data from db to populate cards
@@ -13,22 +14,33 @@ const RecipeList = (props) => {
     <Row className="flex-row">
       {
         // map recipes to return cards, remember to add a key
-        <Card>
-          <Card.Img variant="top" alt="recipe card img" />
-          <Card.Body>
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <Card.Title title={data} />
-            )}
-            <Card.Subtitle>difficulty | prep time | cook time</Card.Subtitle>
-            <ListGroup variant="flush">
-              <ListGroup.Item>ingredient 1</ListGroup.Item>
-              <ListGroup.Item>ingredient 2</ListGroup.Item>
-            </ListGroup>
-            <Card.Text>These are the prep instructions</Card.Text>
-          </Card.Body>
-        </Card>
+        <>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            queriedRecipes.map((recipe) =>{
+              return (
+                <Card>
+              <Card.Img variant="top" alt="recipe card img" />
+              <Card.Body>
+                <Card.Title>{recipe.title}</Card.Title>
+                <Card.Subtitle>
+                  difficulty | prep time | cook time
+                </Card.Subtitle>
+                <ListGroup variant="flush">
+                  {recipe.ingredients.map((ingredient) => {
+                    return (
+                      <ListGroup.Item>{ingredient}</ListGroup.Item>
+                    )
+                  })}
+                </ListGroup>
+                <Card.Text>These are the prep instructions</Card.Text>
+              </Card.Body>
+            </Card>
+              )
+            })
+          )}
+        </>
       }
     </Row>
   );
