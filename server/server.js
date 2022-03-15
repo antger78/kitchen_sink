7cc6ab83aac76187b004218957cdd407a87b625e
l bates childs <labchilds@gmail.com>
2022-03-15 00:28:17 -0400

    added startServer() back into server

--- server/server.js
+++ server/server.js
@@ -8,12 +8,21 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const startServer = async () => {
+  // create a new Apollo server and pass in schema
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
 
-server.applyMiddleware({ app });
+  // start server and apply middleware
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -29,6 +38,5 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
