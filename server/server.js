8a9315eddbbe4e29888019558cea84bb81acfb1f
l bates childs <labchilds@gmail.com>
2022-03-15 00:31:59 -0400

    add comments to server, import auth

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
+
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -27,6 +29,7 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// if production, serve static assets from client/build
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
