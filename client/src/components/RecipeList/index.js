4e80600e682ee8cd67b1b8eb37b5331154092e6a
a.breezy <ambrose.wilkins@gmail.com>
2022-03-15 20:29:07 -0400

    update initial css

--- client/src/components/RecipeList/index.js
+++ client/src/components/RecipeList/index.js
@@ -6,58 +6,58 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./recipeList.css";
 import ReactTextCollapse from "react-text-collapse";
 const TEXT_COLLAPSE_OPTIONS = {
-  collapse: false, // default state when component rendered
-  collapseText: "... show more", // text to show when collapsed
-  expandText: "show less", // text to show when expanded
-  minHeight: 100, // component height when closed
-  maxHeight: 150, // expanded to
-  textStyle: {
-    // pass the css for the collapseText and expandText here
-    color: "blue",
-    fontSize: "20px",
-  },
+	collapse: false, // default state when component rendered
+	collapseText: "... show more", // text to show when collapsed
+	expandText: "show less", // text to show when expanded
+	minHeight: 100, // component height when closed
+	maxHeight: 150, // expanded to
+	textStyle: {
+		// pass the css for the collapseText and expandText here
+		color: "blue",
+		fontSize: "20px",
+	},
 };
 
 const RecipeList = (props) => {
-  const {
-    id,
-    title,
-    difficulty,
-    prepInstructions,
-    prepTime,
-    cookTime,
-    ingredients,
-  } = props;
-  // get data from db to populate cards
-  // const { loading, data } = useQuery(QUERY_RECIPES);
-  // // if data exists, store in queriedRecipes const
-  // const queriedRecipes = data?.recipes || [];
-  // console.log(queriedRecipes);
-  return (
+	const {
+		id,
+		title,
+		difficulty,
+		prepInstructions,
+		prepTime,
+		cookTime,
+		ingredients,
+	} = props;
+	// get data from db to populate cards
+	// const { loading, data } = useQuery(QUERY_RECIPES);
+	// // if data exists, store in queriedRecipes const
+	// const queriedRecipes = data?.recipes || [];
+	// console.log(queriedRecipes);
+	return (
+		<Row className="flex-row">
+			<Card id={id} className="card">
+				{/* <Card.Img variant="top" alt="recipe card img" /> */}
+				<Card.Body>
+					<Card.Title className="cardTitle">{title}</Card.Title>
+					<Card.Text className="cardDifficulty">
+						Difficulty: {difficulty}
+					</Card.Text>
+					<Card.Subtitle>Prep Time: {prepTime}</Card.Subtitle>
+					<Card.Subtitle>Cook Time: {cookTime}</Card.Subtitle>
 
-    <Row className="flex-row">
-      <Card id={id}>
-        {/* <Card.Img variant="top" alt="recipe card img" /> */}
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>Difficulty: {difficulty}</Card.Text>
-          <Card.Subtitle>Prep Time: {prepTime}</Card.Subtitle>
-          <Card.Subtitle>Cook Time: {cookTime}</Card.Subtitle>
+					<ListGroup variant="flush">
+						{ingredients.map((ingredient) => {
+							return <ListGroup.Item>{ingredient}</ListGroup.Item>;
+						})}
+					</ListGroup>
 
-          <ListGroup variant="flush">
-            {ingredients.map((ingredient) => {
-              return <ListGroup.Item>{ingredient}</ListGroup.Item>;
-            })}
-          </ListGroup>
-
-          <ReactTextCollapse options={TEXT_COLLAPSE_OPTIONS}>
-            <Card.Text>Prep Instructions: {prepInstructions}</Card.Text>
-          </ReactTextCollapse>
-        </Card.Body>
-      </Card>
-
-    </Row>
-  );
+					<ReactTextCollapse options={TEXT_COLLAPSE_OPTIONS}>
+						<Card.Text>Prep Instructions: {prepInstructions}</Card.Text>
+					</ReactTextCollapse>
+				</Card.Body>
+			</Card>
+		</Row>
+	);
 };
 
 export default RecipeList;
